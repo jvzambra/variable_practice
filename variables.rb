488a2bf7960bf6e5b2baf5c37cefede533944107
Jorge Zambrana <zee7man@yahoo.com>
2023-07-07 13:43:13 -0600

    Add floats

--- variables.rb
+++ variables.rb
@@ -12,3 +12,10 @@ favorite_number = 7
 puts "My favorite number has always been #{favorite_number}."
 number_of_pets = 2
 puts "I currently have #{number_of_pets} pets."
+#Floats
+bank_account_balance = 1000000.00
+puts "I keep #{bank_account_balance} in the bank at all times."
+monthly_rent = 1000.00
+puts "My rent was once cheap but is now #{monthly_rent}."
+security_deposit = 1200.00
+puts "The deposit for Turing was #{security_deposit}."
