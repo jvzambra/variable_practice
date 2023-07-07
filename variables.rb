72da41c227b937ddbc4fd8ec6805c5329e778fb2
Jorge Zambrana <zee7man@yahoo.com>
2023-07-07 13:37:36 -0600

    Add strings

---
+++ variables.rb
@@ -0,0 +1,7 @@
+# Strings
+name = "Jorge"
+puts "I go by #{name}."
+month_of_birth = "February"
+puts "I was not born in #{month_of_birth}."
+yolo = "You only live once."
+puts "The motto #{yolo} makes absolutely no sense!"
