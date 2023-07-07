f1e8944d111d399f3cc4b1147a1460ba0461dcee
Jorge Zambrana <zee7man@yahoo.com>
2023-07-07 13:39:57 -0600

    Add integers

--- variables.rb
+++ variables.rb
@@ -5,3 +5,10 @@ month_of_birth = "February"
 puts "I was not born in #{month_of_birth}."
 yolo = "You only live once."
 puts "The motto #{yolo} makes absolutely no sense!"
+# Integers
+age = 35
+puts "I am #{age} years old."
+favorite_number = 7
+puts "My favorite number has always been #{favorite_number}."
+number_of_pets = 2
+puts "I currently have #{number_of_pets} pets."
